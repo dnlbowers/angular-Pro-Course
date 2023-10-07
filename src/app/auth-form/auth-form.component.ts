65215c237686b50fbd72ee9459083e9ef0a1cf99
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-10-07 09:01:26 +0200

    Add email native element

--- src/app/auth-form/auth-form.component.ts
+++ src/app/auth-form/auth-form.component.ts
@@ -6,7 +6,7 @@ import {
   ContentChildren,
   QueryList,
   AfterContentInit,
-  ViewChildren, ChangeDetectorRef
+  ViewChildren, ChangeDetectorRef, ElementRef, ViewChild
 } from '@angular/core';
 import {User} from "./auth-form.interface";
 import {AuthRememberComponent} from "./auth-remember.component";
@@ -14,13 +14,16 @@ import {AuthMessageComponent} from "./auth-message.component";
 
 @Component({
   selector: 'auth-form',
+  styles: [`
+    .email { border-color: #9f72e6;}
+  `],
   template: `
     <div>
       <form (ngSubmit)="onSubmit(form.value)" #form="ngForm">
         <ng-content select="h3"></ng-content>
         <label>
           Email address
-          <input type="email" name="email" ngModel>
+          <input type="email" name="email" ngModel #email>
         </label>
         <label>
           Password
@@ -28,25 +31,24 @@ import {AuthMessageComponent} from "./auth-message.component";
         </label>
         <ng-content select="auth-remember"></ng-content>
         <auth-message [style.display]="(showMessage ? 'inherit' : 'none')"></auth-message>
-        <auth-message [style.display]="(showMessage ? 'inherit' : 'none')"></auth-message>
-        <auth-message [style.display]="(showMessage ? 'inherit' : 'none')"></auth-message>
         <ng-content select="button"></ng-content>
       </form>
     </div>
   `,
-  styles: [
-  ]
 })
 export class AuthFormComponent implements AfterContentInit, AfterViewInit {
 
   showMessage!: boolean;
-
+  @ViewChild('email') email: ElementRef | null= null
   @ViewChildren(AuthMessageComponent) message: QueryList<AuthMessageComponent> | null = null;
   @ContentChildren(AuthRememberComponent) remember!: QueryList<AuthRememberComponent>;
   @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
 
   constructor(private cd: ChangeDetectorRef) {}
   ngAfterViewInit() {
+    this.email?.nativeElement.setAttribute('placeholder', 'Enter your email');
+    this.email?.nativeElement.classList.add('email');
+    this.email?.nativeElement.focus();
     if(this.message) {
       this.message?.forEach((message) => {
           message.days = 30
